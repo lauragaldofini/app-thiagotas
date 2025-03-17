87e5756a676b874622508a278a0a716892673053
231100 <231100@CPD02-M15>
2025-03-17 15:51:22 -0300

    oi

---
+++ routes.php
@@ -0,0 +1,91 @@
+ <?php
+
+  use App\Controller\{
+    AlunoController,
+    InicialController,
+    LoginController,
+    AutorController,
+    CategoriaController,
+    LivroController,
+    EmprestimoController,
+  };
+
+  $url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+  switch($url)
+  {
+    case '/':
+        InicialController::index();
+        break;
+
+
+    case '/login':
+            LoginController::index();
+            break;
+
+    case'/logout';
+         LoginController::logout();
+         break;
+
+    case '/aluno':
+        AlunoController::index();
+        break;
+
+    case '/aluno/cadastro':
+        AlunoController::cadastro();
+        break;
+
+    case '/aluno/delete':
+        AlunoController::delete();
+        break;
+
+    case '/autor':
+        AutorController::index();
+        break;
+
+    case '/autor/cadastro':
+        AutorController::cadastro();
+        break;
+
+    case '/autor/delete':
+        AutorController::delete();
+        break;
+
+    case '/categoria':
+        CategoriaController::index();
+        break;
+
+    case '/categoria/cadastro';
+        CategoriaController::cadastro();
+        break;
+
+    case '/categoria/delete';
+        CategoriaController::delete();
+        break;
+
+    case '/livros';
+        LivrosController::index();
+        break;
+
+    case '/livros/cadastro';
+        LivrosController::cadastro();
+         break;
+  
+    case '/livros/delete';
+        LivrosController::delete();
+         break;
+ 
+     case '/emprestimo';
+        EmprestimoController::index();
+        break;
+  
+    case '/emprestimo/cadastro';
+        EmprestimoController::cadastro();
+        break;
+
+          
+    case '/emprestimo/delete';
+        EmprestimoController::delete();
+        break;
+          
+  }
