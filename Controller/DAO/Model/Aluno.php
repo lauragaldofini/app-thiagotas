c68b2037d7fadb101e4aac021dd0ee127c12a055
231100 <231100@CPD02-M15>
2025-03-24 15:40:29 -0300

    laura linda

---
+++ Controller/DAO/Model/Aluno.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Model;
+
+use App\DAO\AlunoDAO;
+use Exception;
+
+final class Aluno Extends Model
+{
+    public ?int $Id = null;
+
+    public ?string $Nome
+    {
+        set
+        {
+            if (strlen ($value) < 3)
+            throw new Exception("Nome deve ter no mínimo 3 caracteres.");
+            $this->Nome = $value;
+        }
+        
+        get=> $this->Nome ?? null;
+    }
+
+    public ?string $RA
+    {
+
+        set
+        {
+            if(empty($value))
+                throw new Exception("Preencha o RA");
+
+                $this-> RA - $value;
+
+        }
+        
+        get => $this->RA ?? null;
+    }
+
+    public ?string $Curso
+    {
+         
+        set
+        {
+            if(strlen($value)< 3)
+               throw new Exception("Curso deve ter no mínimo 3 caracteres.");
+
+               $this->cURSO = $value;
+        }
+
+        get=> $this->Curso ?? null;
+    }
+
+    function save() : Aluno
+    {
+          return new AlunoDAO()->save($this);
+    }
+    function gent(int $Id) : ?Aluno
+    {
+        return new AlunoDAO()->selectById($id);
+    } 
+
+    function getAllRows() : array
+    {
+        $this->rows = new AlunoDAO()->select();
+
+        return $this->rows;
+    }
+
+    function delete(int $id) : bool
+    {
+        return new AlunoDAO()->delete($this);
+    }
+}
