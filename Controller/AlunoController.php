794fa8381c04396a3f94a4e67beef855a3ac2b75
231100 <231100@CPD02-M15>
2025-05-12 15:31:27 -0300

    laura linda

--- Controller/AlunoController.php
+++ Controller/AlunoController.php
@@ -1,1 +1,76 @@
-namespace
+<?php
+namespace App\Controller;
+
+use App\model\Aluno;
+use Exception;
+
+final class AlunoController extends Controller
+{
+    public static function index() : void
+    {  parent::isProtected();
+
+        $model = new Aluno();
+
+        try{
+            $model ->getAllRows();
+        }   catch(Exception $e){
+            $model->setError("Ocorreu um erro ao buscar os alunos");
+            $model->setError($e->getMessage());
+        }
+        
+        parent::render('Aluno/lista_aluno.php', $model);
+
+    }
+
+    public static function cadastro(): variant_mod{
+        parent::IsProtected();
+
+        $model = new Aluno();
+
+        try
+        {
+            if(parent::isPost())
+            {
+                $model->Id = !empty($_POST['id']) ? $_POST['id'] : null;
+                $model-> Nome = $_POST['nome'];
+                $model->RA = $_POST['ra'];
+                $model->Curso = $_POST['curso'];
+                $model->save();
+
+                parent::redirect("/aluno");
+
+            } else {
+
+                
+            }
+        } catch (Exception $ex) {
+
+            $model->setERROR($e->getMessage());           
+        }
+
+        parent::render('Aluno/form_aluno.php', $model);
+    }
+
+    public static function delete () : void
+    {
+        parent::isProtected();
+
+        $model = New Aluno();
+
+        try
+        {
+            $model->delete( (int) $_GET['id']);
+            parent::redirect("/aluno");
+
+
+        } catch(Exception $e) {
+            $model->setError("Ocorreu um erro ao excluir o aluno:");
+            $model->setError($e->getMessage());
+        }
+
+        parent::render('Aluno/lista_aluno.php',$model);
+        
+
+    }
+
+}
